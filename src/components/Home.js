e5f0972722ffb9a64435800bd09d11d24a00fcd5
Arthur <arthuroaraujo@gmail.com>
2022-09-08 17:10:35 -0300

    Req3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import ShoppingCart from './ShoppingCart';
 
 class Home extends Component {
   render() {
     return (
       <div data-testid="home-initial-message">
         Digite algum termo de pesquisa ou escolha uma categoria.
+        {/* <button
+          type="button"
+          data-testid="shopping-cart-button"
+          to="/shoppingCart"
+        >
+          Seu carrinho está vazio
+
+        </button> */}
+        <Link
+          data-testid="shopping-cart-button"
+          to="/shoppingCart"
+        >
+          {' '}
+          Carrinho de Compras
+
+        </Link>
       </div>
     );
   }
---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import Home from './Home';
+
+class ShoppingCart extends Component {
+  render() {
+    return (
+      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    );
+  }
+}
+
+export default ShoppingCart;
