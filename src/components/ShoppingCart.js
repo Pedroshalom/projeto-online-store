57b821f54ef3685b28c4164d6ce9e5de5d642966
Pedro <pedroshalommrn@hotmail.com>
2022-09-14 14:14:37 -0300

    terminando o req 8

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-
+import { readItens } from '../services/api';
 class ShoppingCart extends Component {
+  state = {
+    cartItems: [],
+  };
+  componentDidMount() {
+    const request = readItens();
+    this.setState({ cartItems: request });
+  }
   render() {
+    const { cartItems } = this.state;
     return (
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      <div>
+        { !cartItems
+          ? (
+            <p data-testid="shopping-cart-empty-message">
+              Seu carrinho está vazio
+            </p>)
+          : cartItems.map((e) => (
+            <div key={ e.id }>
+              <h3 data-testid="shopping-cart-product-name">{e.title}</h3>
+              <h4>{e.price}</h4>
+              <h4 data-testid="shopping-cart-product-quantity">{e.quantity}</h4>
+            </div>
+          ))}
+      </div>
     );
   }
 }
-
 export default ShoppingCart;
